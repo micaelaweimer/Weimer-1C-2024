3ff2e9be1297c272bc2a1154b02d5fa77e9b9e0c
micaelaweimer <micaela.weimer@ingenieria.uner.edu.ar>
2024-06-20 16:52:19 -0300

    Programa completo con documentacion

--- firmware/projects/ProyectoIntegrador/main/ProyectoIntegrador.c
+++ firmware/projects/ProyectoIntegrador/main/ProyectoIntegrador.c
@@ -42,12 +42,12 @@
  * | 	 VCC	 	| 	 +3.3V		|
  * | 	ETRADA	 	| 	  CH1		|
  * | 	 GND	 	| 	  GND		|
- * 
+ *
  * |      LED       |   ESP32   	|
  * |:--------------:|:--------------|
  * | 	SALIDA	 	| 	GPIO_18		|
  * | 	 GND	 	| 	  GND		|
- * 
+ *
  * @section changelog Changelog
  *
  * |   Date	    | Description        |
@@ -76,23 +76,56 @@
 #include "LDR.h"
 #include "analog_io_mcu.h"
 /*==================[macros and definitions]=================================*/
-// motor izquierdo B
-#define MIA GPIO_16 // amarillo
-#define MIB GPIO_22 // blanco
-// motor derecho A
-#define MDA GPIO_17 // violeta
-#define MDB GPIO_23 // gris
+/*!
+ * @brief definicion de gpio para motor
+ */
+#define MIA GPIO_16
+/*!
+ * @brief definicion de gpio para motor
+ */
+#define MIB GPIO_22 
+/*!
+ * @brief definicion de gpio para motor
+ */
+#define MDA GPIO_17
+/*!
+ * @brief definicion de gpio para motor
+ */
+#define MDB GPIO_23 
 
-#define PERIODO 1000000 // periodo lectura del hc sr04
+/*!
+ * @brief periodo de lectura del sensor hc sr04
+ */
+#define PERIODO 25000
+/*!
+ * @brief grados de giro para el servo
+ */
 #define giroDer -90
+/*!
+ * @brief grados de giro para el servo
+ */
 #define giroIzq 90
+/*!
+ * @brief grados de giro para el servo
+ */
 #define posCentral 0
 
+/*!
+ * @brief periodo para bluetooth
+ */
 #define CONFIG_BLINK_PERIOD 500
-#define LED_BT LED_1
 
-#define PERIODO 500000 // periodo lectura del LDR
+/*!
+ * @brief periodo de lectura del LDR
+ */
+#define PERIODO_LED 500000
+/*!
+ * @brief definicion del gpio para led
+ */
 #define GPIO_LED GPIO_18
+/*!
+ * @brief lux normal para comparacion
+ */
 #define LUX_NORMAL 500
 /*==================[internal data definition]===============================*/
 typedef struct
@@ -104,19 +137,17 @@ typedef struct
 /*!
  * @brief Variable booleana para control de encendido y apagado del programa
  */
-bool control = true;
+volatile bool control = false;
 
+/*!
+ * @brief vector de pwm para control de motores
+ */
 pwm_out_t pwm_motores[2];
 
 /*!
  * @brief Distancia en cm leida por el sensor de ultrasonido
  */
 uint16_t distancia;
-
-/*!
- * @brief Almacena el estado del robot
- */
-char estado[500];
 
 /*!
  * @brief Almacena el valor de intensidad leido por el sensor
@@ -166,13 +197,6 @@ void read_data(uint8_t *data, uint8_t length)
     }
 }
 
-//     // Si esta encendido
-//     if (control == true)
-//     {
-//         BleSendString(estado); // Envio el estado del robot a la app del celular
-//     }
-// }
-
 /*!
  * @brief Controla el giro de los motores
  * @param dir Direccion de debe seguir
@@ -189,8 +213,8 @@ void controlRuedas(char dir)
         PWMOn(pwm_motores[1]);
 
         // Velocidad
-        PWMSetDutyCycle(pwm_motores[0], 50);
-        PWMSetDutyCycle(pwm_motores[1], 50);
+        PWMSetDutyCycle(pwm_motores[0], 60);
+        PWMSetDutyCycle(pwm_motores[1], 60);
         break;
 
     case 'F': // frenar
@@ -209,8 +233,8 @@ void controlRuedas(char dir)
         PWMOn(pwm_motores[1]);
 
         // velocidad
-        PWMSetDutyCycle(pwm_motores[0], 45); // rueda izq
-        PWMSetDutyCycle(pwm_motores[1], 60); // rueda der
+        PWMSetDutyCycle(pwm_motores[0], 35); // rueda izq
+        PWMSetDutyCycle(pwm_motores[1], 65); // rueda der
         break;
 
     case 'I': // giro izquierda
@@ -221,8 +245,8 @@ void controlRuedas(char dir)
         PWMOn(pwm_motores[1]);
 
         // velocidad
-        PWMSetDutyCycle(pwm_motores[0], 60); // rueda izq
-        PWMSetDutyCycle(pwm_motores[1], 45); // rueda der
+        PWMSetDutyCycle(pwm_motores[0], 65); // rueda izq
+        PWMSetDutyCycle(pwm_motores[1], 35); // rueda der
         break;
 
     default:
@@ -235,6 +259,7 @@ void controlRuedas(char dir)
  */
 void controlDistancia(void *pvParameter)
 {
+
     while (1)
     {
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
@@ -242,47 +267,51 @@ void controlDistancia(void *pvParameter)
         {
             distancia = HcSr04ReadDistanceInCentimeters();
 
-            uint16_t distDer, distIzq;
+            static uint16_t distDer, distIzq;
 
             if (distancia > 15)
             {
-               // sprintf(estado, "*EAvanzando\n*");
+                BleSendString("*EAvanzando\n*");
                 controlRuedas('A');
             }
             else if (distancia <= 15)
             {
                 // apago motores
-                //sprintf(estado, "*EFreno\n*");
+                BleSendString("*EFreno\n*");
                 controlRuedas('F');
 
                 ServoMove(SERVO_2, giroDer); // se queda en esta posicion
-                distDer = HcSr04ReadDistanceInCentimeters();
                 vTaskDelay(850 / portTICK_PERIOD_MS);
+                distDer = HcSr04ReadDistanceInCentimeters();
                 ServoMove(SERVO_2, posCentral);
                 vTaskDelay(600 / portTICK_PERIOD_MS);
                 ServoMove(SERVO_2, giroIzq);
-                distIzq = HcSr04ReadDistanceInCentimeters();
                 vTaskDelay(850 / portTICK_PERIOD_MS);
+                distIzq = HcSr04ReadDistanceInCentimeters();
                 ServoMove(SERVO_2, posCentral);
                 vTaskDelay(600 / portTICK_PERIOD_MS);
 
                 if (distDer > distIzq)
                 {
                     // grio a la derecha
-                   // sprintf(estado, "*EGiro a la derecha\n*");
+                    BleSendString("*EGiro a la derecha\n*");
                     controlRuedas('D');
-                    vTaskDelay(350 / portTICK_PERIOD_MS);
+                    vTaskDelay(650 / portTICK_PERIOD_MS);
                     controlRuedas('A');
                 }
-                else
+                else if (distDer < distIzq)
                 {
                     // giro a la izquierda
-                    //sprintf(estado, "*EGiro a la izquierda\n*");
+                    BleSendString("*EGiro a la izquierda\n*");
                     controlRuedas('I');
-                    vTaskDelay(350 / portTICK_PERIOD_MS);
+                    vTaskDelay(650 / portTICK_PERIOD_MS);
                     controlRuedas('A');
                 }
             }
+        }
+        else
+        {
+            controlRuedas('F');
         }
     }
 }
@@ -301,12 +330,12 @@ void controlIntensidad(void *pvParameter)
             // 0 significa máxima resistencia (menos luz) y 1023 mínima resistencia (más luz)
             intensidad = LDR_ReadIntensity();
 
-            if (intensidad > LUX_NORMAL)
+            if (intensidad < LUX_NORMAL)
             {
                 // PRENDO UNA LUZ (LED)
                 GPIOOn(GPIO_LED);
             }
-            else if (intensidad < LUX_NORMAL)
+            else if (intensidad > LUX_NORMAL)
             {
                 // APAGO LA LUZ
                 GPIOOff(GPIO_LED);
@@ -318,11 +347,10 @@ void controlIntensidad(void *pvParameter)
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
-    // inicializo hc sr04, servo y LDR
+    // inicializo hc sr04, servo, LDR y leds
     HcSr04Init(GPIO_3, GPIO_2);
     ServoInit(SERVO_2, GPIO_21);
     LDRInit(CH1);
-
     LedsInit();
 
     // inicializo Bluetooth
@@ -331,7 +359,7 @@ void app_main(void)
         read_data};
     BleInit(&ble_configuration);
 
-    /* Configuracion e inicializacion del timer A*/
+    /* Configuracion e inicializacion del timer A */
     timer_config_t timer_distancia = {
         .timer = TIMER_A,
         .period = PERIODO,
@@ -339,10 +367,10 @@ void app_main(void)
         .param_p = NULL};
     TimerInit(&timer_distancia);
 
-    /* Configuracion e inicializacion del timer B*/
+    /* Configuracion e inicializacion del timer B */
     timer_config_t timer_oscuridad = {
         .timer = TIMER_B,
-        .period = PERIODO,
+        .period = PERIODO_LED,
         .func_p = FuncTimerB_Intesnidad,
         .param_p = NULL};
     TimerInit(&timer_oscuridad);
@@ -382,31 +410,31 @@ void app_main(void)
     PWMInit(pwm_motores[0], MIA, 50);
     PWMInit(pwm_motores[1], MDA, 50);
 
-    // Los leds cambian segun el estado del BLE:
-    // Conectado prende led verde, si se desconecta el led amarillo y si se apaga el led rojo
-    // while (1)
-    // {
-    //     vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-    //     switch (BleStatus())
-    //     {
-    //     case BLE_OFF:
-    //         LedOn(LED_3);
-    //         break;
-    //     case BLE_DISCONNECTED:
-    //         LedOff(LED_1);
-    //         LedToggle(LED_2);
-    //         break;
-    //     case BLE_CONNECTED:
-    //         LedOff(LED_2);
-    //         LedOn(LED_1);
-    //         break;
-    //     }
-    // }
-
     xTaskCreate(&controlDistancia, "controlDistancia", 2048, NULL, 5, &control_distancia_handle);
     xTaskCreate(&controlIntensidad, "controlIntensidad", 2048, NULL, 5, &control_intensidad_handle);
 
     TimerStart(timer_distancia.timer);
     TimerStart(timer_oscuridad.timer);
+
+    // Los leds cambian segun el estado del BLE:
+    // Conectado prende led verde, si se desconecta el led amarillo y si se apaga el led rojo
+    while (1)
+    {
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        switch (BleStatus())
+        {
+        case BLE_OFF:
+            LedOn(LED_3);
+            break;
+        case BLE_DISCONNECTED:
+            LedOff(LED_1);
+            LedToggle(LED_2);
+            break;
+        case BLE_CONNECTED:
+            LedOff(LED_2);
+            LedOn(LED_1);
+            break;
+        }
+    }
 }
 /*==================[end of file]============================================*/
